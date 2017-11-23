b33bca8e74a65b26e736c5b5382c78e152718444
MarloneA <marlone911@gmail.com>
2017-11-23 23:49:23 +0300

    Improved pylint tests

--- api_1_0/api.py
+++ api_1_0/api.py
@@ -2,46 +2,56 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = [{"name":"Marlone","email":"marlone911@gmail.com"},{"name":"Prestone","email":"pres911@gmail.com"},{"name":"Daltone","email":"dalt911@gmail.com"}]
-events = [{"title":"Rumba","category":"dance"},{"title":"Flamenco","category":"mariachi"},{"title":"Picado","category":"tango"}]
-logged_in = [{"name":"Brian"},{"name":"Ryu"},{"name":"Rio"}]
-logged_out = [{"name":"Sheila"},{"name":"Lisa"},{"name":"Jane"}]
-updated_passwords = [{"password":"123"},{"password":"654"},{"password":"grunt2117"}]
-RSVP = [{"name":"Uno"},{"name":"miho"},{"name":"punto"}]
+users = [{"name":"Marlone", "email":"mar1@gmail.com"}, {"name":"Pres", "email":"pres911@gmail.com"}]
+events = [{"title":"Rumba", "category":"dance"}, {"title":"Flamenco", "category":"mariachi"}]
+logged_in = [{"name":"Brian"}, {"name":"Ryu"}, {"name":"Rio"}]
+logged_out = [{"name":"Sheila"}, {"name":"Lisa"}, {"name":"Jane"}]
+updated_passwords = [{"password":"123"}, {"password":"654"}, {"password":"grunt2117"}]
+RSVP = [{"name":"Uno"}, {"name":"miho"}, {"name":"punto"}]
 
 
 #create a new user
-@app.route('/api/auth/register', methods = ['POST'])
+@app.route('/api/auth/register', methods=['POST'])
 def create_user():
-
-    #creates a new user and adds them to the list of users
-    user = {'name':request.json['name'],'email':request.json['email']}
+    """
+    Creates a user account
+    """
+    user = {'name':request.json['name'], 'email':request.json['email']}
     users.append(user)
 
     return jsonify({"users":users})
 
 #login a user
-@app.route('/api/auth/login', methods = ['POST'])
+@app.route('/api/auth/login', methods=['POST'])
 def login_user():
+    """
+    Logs in a user
+    """
 
-    login_user = {'username':request.json['username']}
-    logged_in.append(login_user)
+    log = {'username':request.json['username']}
+    logged_in.append(log)
 
     return jsonify({"logged_in":logged_in})
 
 
 #logs out a user
-@app.route('/api/auth/logout', methods = ['POST'])
+@app.route('/api/auth/logout', methods=['POST'])
 def logout_user():
+    """
+    Logs out a user
+    """
 
-    logout_user = {'username':request.json['username']}
-    logged_out.append(logout_user)
+    logout = {'username':request.json['username']}
+    logged_out.append(logout)
 
     return jsonify({"logged_out":logged_out})
 
 #resets password
-@app.route('/api/auth/reset-password', methods = ['POST'])
+@app.route('/api/auth/reset-password', methods=['POST'])
 def reset_password():
+    """
+    Resets password
+    """
 
     password_reset = {'password':request.json['password']}
     updated_passwords.append(password_reset)
@@ -49,26 +59,35 @@ def reset_password():
     return jsonify({"updated_passwords":updated_passwords})
 
 #creates an event
-@app.route('/api/events', methods = ['POST'])
+@app.route('/api/events', methods=['POST'])
 def create_event():
+    """
+    Creates an Event
+    """
 
-    event = {'title':request.json['title'],'category':request.json['category']}
+    event = {'title':request.json['title'], 'category':request.json['category']}
     events.append(event)
 
     return jsonify({"events":events})
 
 #updates an event
-@app.route('/api/events/<categoryId>', methods = ['PUT'])
-def update_event(categoryId):
+@app.route('/api/events/<eventId>', methods=['PUT'])
+def update_event(eventId):
+    """
+    Updates an Event
+    """
 
-    event = [event for event in events if event["title"] == categoryId]
+    event = [event for event in events if event["title"] == eventId]
     event[0]["title"] = request.json["title"]
 
     return jsonify({"events":event[0]})
 
 #deletes an event
-@app.route('/api/events/<eventId>', methods = ['DELETE'])
+@app.route('/api/events/<eventId>', methods=['DELETE'])
 def delete_event(eventId):
+    """
+    Deletes an event
+    """
 
     event = [event for event in events if event["title"] == eventId]
     events.remove(event[0])
@@ -77,31 +96,27 @@ def delete_event(eventId):
 
 
 #retrieves all events
-@app.route('/api/events', methods = ['GET'])
-def retrieve_all_events():
+@app.route('/api/events', methods=['GET'])
+def retrieve_events():
+    """
+    Retrieves events
+    """
+
     return jsonify({"events":events})
 
-#retrieves one event
-@app.route('/api/events/<string:eventId>', methods = ['GET'])
-def retrieve_one_event(eventId):
-
-    event = [event for event in events if event["title"] == eventId]
-
-    return jsonify({"events":event[0]})
-
 #allows user to RSVP
-@app.route('/api/event/<eventId>/rsvp', methods = ['POST'])
+@app.route('/api/event/<eventId>/rsvp', methods=['POST'])
 def rsvp_event(eventId):
+    """
+    Allows a user to RSVP to an event
+    """
 
-    reserved_guests = {'name':request.json['name'],'event':request.json['event']}
+    reserved_guests = {'name':request.json['name'], 'event':request.json['event']}
     RSVP.append(reserved_guests)
 
     return jsonify({"RSVP":RSVP})
 
-#retrieves all Reserved guests
-@app.route('/api/event/<eventId>/rsvp', methods = ['GET'])
-def retrieve_all_guests(eventId):
-    return jsonify({"RSVP":RSVP})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
