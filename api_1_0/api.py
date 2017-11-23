c6e81da4730efcf06197716bb4abff041e3e0589
MarloneA <marlone911@gmail.com>
2017-11-23 08:33:56 +0300

    configure API endpoints to send simple message

--- api_1_0/api.py
+++ api_1_0/api.py
@@ -1,49 +1,56 @@
-@api.route('/api/auth/register', methods = ['POST'])
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/auth/register', methods = ['POST'])
 def create_user():
     return jsonify({'message':'registration succesful'})
 
 #login a user
-@api.route('/api/auth/login', methods = ['POST'])
+@app.route('/api/auth/login', methods = ['POST'])
 def login_user():
     return jsonify({'message':'user logged in'})
 
 #logs out a user
-@api.route('/api/auth/login', methods = ['POST'])
+@app.route('/api/auth/login', methods = ['POST'])
 def logout_user():
     return jsonify({'message':'user logged out'})
 
 #resets password
-@api.route('/api/auth/reset-password', methods = ['POST'])
+@app.route('/api/auth/reset-password', methods = ['POST'])
 def reset_password():
     return jsonify({'message':'password reset'})
 
 
 #creates an event
-@api.route('/api/events', methods = ['POST'])
+@app.route('/api/events', methods = ['POST'])
 def create_event():
     return jsonify({'message':'event created'})
 
 #updates an event
-@api.route('/api/events/<eventId>', methods = ['PUT'])
-def update_event(user_id):
+@app.route('/api/events/<eventId>', methods = ['PUT'])
+def update_event(eventId):
     return jsonify({'message':'event updated'})
 
 #deletes an event
-@api.route('/api/events/<eventId>', methods = ['DELETE'])
-def delete_event(user_id):
+@app.route('/api/events/<eventId>', methods = ['DELETE'])
+def delete_event(eventId):
     return jsonify({'message':'event deleted'})
 
 #retrieves an event
-@api.route('/api/events', methods = ['GET'])
+@app.route('/api/events', methods = ['GET'])
 def retrieve_event():
     return jsonify({"message":"retrieved event"})
 
 #allows user to RSVP
-@api.route('/api/event/<eventId>/rsvp', methods = ['POST'])
-def rsvp_event(user_id):
+@app.route('/api/event/<eventId>/rsvp', methods = ['POST'])
+def rsvp_event(eventId):
     return jsonify({"message":"Reserved event"})
 
 #retrieves an event
-@api.route('/api/event/<eventId>/rsvp', methods = ['GET'])
-def retrieve_all_guests():
+@app.route('/api/event/<eventId>/rsvp', methods = ['GET'])
+def retrieve_all_guests(eventId):
     return jsonify({"message":"retrieved guests in attendance"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Flask
-
-def create_app(config_name):
-
-    app = Flask(__name__)
-
-
-    return app
