5cb3cfd03e57d89d2ace833b30d47b0cab814705
MarloneA <marlone911@gmail.com>
2017-11-28 08:50:26 +0300

    [fix] add files to be ignored by git

--- app/models.py
+++ app/models.py
@@ -1,7 +1,12 @@
 
-users = [{"name":"Marlone", "email":"mar1@gmail.com"}, {"name":"Pres", "email":"pres911@gmail.com"}]
-events = [{"title":"Rumba", "category":"dance"}, {"title":"Flamenco", "category":"mariachi"}]
-logged_in = [{"name":"Brian"}, {"name":"Ryu"}, {"name":"Rio"}]
+users = [{"id":3, "name":"Marlone","email":"mar1@gmail.com", "password":"1234"}, {"id":34, "name":"Pres", "email":"pres911@gmail.com", "password":"3410"}]
+
+events = [{"id":32, "title":"Rumba", "category":"dance"}, {"id":64, "title":"Flamenco", "category":"mariachi"}]
+
+logged_in = [{"email":"rian@yahoo.com"}, {"email":"ryu@green.com"}, {"email":"rio@net.com"}]
+
 logged_out = [{"name":"Sheila"}, {"name":"Lisa"}, {"name":"Jane"}]
+
 updated_passwords = [{"password":"123"}, {"password":"654"}, {"password":"grunt2117"}]
+
 RSVP = [{"name":"Uno"}, {"name":"miho"}, {"name":"punto"}]
