439af4a3d851981ced28340c33d606eaacdca7ea
MarloneA <marlone911@gmail.com>
2018-01-03 14:09:32 +0300

    add base tests

---
+++ tests/base.py
@@ -0,0 +1,30 @@
+from app import app
+import db
+from flask_testing import TestCase
+import json
+
+
+class BaseTestCase(TestCase):
+    def create_app(self):
+        """
+        Create an instance of the app with the testing configuration
+        :return:
+        """
+        app.config.from_object('app.config.Testing')
+        return app
+
+    def setUp(self):
+        """
+        Create the database
+        :return:
+        """
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        """
+        Drop the database tables and also remove the session
+        :return:
+        """
+        db.session.remove()
+        db.drop_all()
