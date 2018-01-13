41694e7ec47b30b0e76a73bff7de1d65b0ba4a8d
MarloneA <marlone911@gmail.com>
2018-01-13 15:00:11 +0300

    [fix] fix issues that where causing production to fail

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ import jwt
 from .models import User, Event
 
 
-from config import app_config
+from instance.config import app_config
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
