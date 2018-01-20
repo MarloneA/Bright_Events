43a198f6fb435fa5175f23234284d014504330e3
MarloneA <marlone911@gmail.com>
2018-01-20 13:27:36 +0300

    [fix] create db environments

--- app/__init__.py
+++ app/__init__.py
@@ -15,12 +15,12 @@ from .models import User, Event, BlackListToken
 
 
 
-from instance.config import app_config
+from .config import app_config
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    # app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
