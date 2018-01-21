589cd9b9eb6eb740de63c7a786aaa27fa6b9f4f6
MarloneA <marlone911@gmail.com>
2018-01-21 21:25:14 +0300

    [fix] troubleshoot type error on push

--- manage.py
+++ manage.py
@@ -32,21 +32,22 @@ def test(coverage=False):
 
     tests = unittest.TestLoader().discover('tests')
     test_results = unittest.TextTestRunner(verbosity=2).run(tests)
+
     if test_results.wasSuccessful():
+
+        if COV:
+            COV.stop()
+            COV.save()
+            print('Coverage Summary:')
+            COV.report()
+            basedir = os.path.abspath(os.path.dirname(__file__))
+            covdir = os.path.join(basedir, 'tmp/coverage')
+            COV.html_report(directory=covdir)
+            print('HTML version: file://%s/index.html' % covdir)
+            COV.erase()
+
         return 0
     return 1
-
-    if COV:
-        COV.stop()
-        COV.save()
-        print('Coverage Summary:')
-        COV.report()
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, 'tmp/coverage')
-        COV.html_report(directory=covdir)
-        print('HTML version: file://%s/index.html' % covdir)
-        COV.erase()
-
 
 if __name__ == '__main__':
     manager.run()
