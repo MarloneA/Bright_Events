4d648e9509850b1978a13be528d08b9ed0cd2d94
Marlone Akidiva <marlonea@Marlones-MacBook-Pro.local>
2018-03-14 19:01:58 +0300

    [fix] change password hash length

--- app/models.py
+++ app/models.py
@@ -46,7 +46,7 @@ class User(db.Model):
     public_id = db.Column(db.Integer,primary_key=True, autoincrement=True, unique=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(300), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
 
     events = db.relationship('Event', secondary='reservations',  backref='user', lazy='dynamic')
 
